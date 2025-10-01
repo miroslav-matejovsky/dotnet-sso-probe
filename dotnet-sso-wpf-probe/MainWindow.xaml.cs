adc01fd4ecb885651aa300527bd3a11f378fad44
Miroslav Matejovsky <miroslav.matejovsky@vissim.no>
2025-10-01 04:23:12 +0200

    wam flow

--- dotnet-sso-wpf-probe/MainWindow.xaml.cs
+++ dotnet-sso-wpf-probe/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using Microsoft.Identity.Client;
+using Microsoft.Identity.Client.Broker;
 using Serilog;
 
 namespace dotnet_sso_wpf_probe
@@ -26,10 +28,65 @@ namespace dotnet_sso_wpf_probe
             this.Closed += MainWindow_Closed;
         }
 
-        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
         {
-            StatusTextBlock.Text = "Logged in";
             Log.Information("Login clicked");
+
+            var scopes = new[] { "User.Read" };
+
+            BrokerOptions options = new BrokerOptions(BrokerOptions.OperatingSystems.Windows);
+            options.Title = "My Awesome Application";
+
+            var wih = new System.Windows.Interop.WindowInteropHelper(this);
+            var hWnd = wih.Handle;
+            
+            IPublicClientApplication app =
+                PublicClientApplicationBuilder.Create("YOUR_CLIENT_ID")
+                    .WithDefaultRedirectUri()
+                    .WithParentActivityOrWindow(() => hWnd)
+                    .WithBroker(options)
+                    .Build();
+
+            AuthenticationResult result = null;
+            
+            // Try to use the previously signed-in account from the cache
+            IEnumerable<IAccount> accounts = await app.GetAccountsAsync();
+            IAccount existingAccount = accounts.FirstOrDefault();
+
+            try
+            {    
+                if (existingAccount != null)
+                {
+                    result = await app.AcquireTokenSilent(scopes, existingAccount).ExecuteAsync();
+                }
+                // Next, try to sign in silently with the account that the user is signed into Windows
+                else
+                {    
+                    result = await app.AcquireTokenSilent(scopes, PublicClientApplication.OperatingSystemAccount)
+                        .ExecuteAsync();
+                }
+            }
+// Can't get a token silently, go interactive
+            catch (MsalUiRequiredException ex)
+            {
+                try
+                {
+                    result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+                } catch (Exception iex)
+                {
+                    Log.Error("Interactive authentication failed: {Error}", iex.Message);
+                    StatusTextBlock.Text = "Authentication failed";
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Authentication failed: {Error}", ex.Message);
+                StatusTextBlock.Text = "Authentication failed";
+                return;
+            }
+
+            StatusTextBlock.Text = "Logged in";
         }
 
         private void LogoutButton_Click(object sender, RoutedEventArgs e)
